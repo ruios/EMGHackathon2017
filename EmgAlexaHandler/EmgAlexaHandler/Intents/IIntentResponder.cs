bcc569723127c78dc613fa20939094524d752ef3
sebastian.wallmark <sebastian.wallmark@studentum.se>
2017-07-05 09:41:10 +0200

    intent handling base

---
+++ EmgAlexaHandler/EmgAlexaHandler/Intents/IIntentResponder.cs
@@ -0,0 +1,36 @@
+﻿using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+
+namespace EmgAlexaHandler.Intents
+{
+    public interface IIntentResponder
+    {
+        bool CanRespond(string name);
+        IOutputSpeech GetResponse(IntentRequest intentRequest);
+    }
+
+    public class SearchIntentResponder : IIntentResponder
+    {
+        public bool CanRespond(string name)
+        {
+            return name == "SearchIntent";
+        }
+
+        public IOutputSpeech GetResponse(IntentRequest intentRequest)
+        {
+            var education = intentRequest.Intent.Slots["Education"].Value;
+            var city = intentRequest.Intent.Slots["City"].Value;
+
+            //log.LogLine($"All slots: {string.Join(",", intentRequest.Intent.Slots)}");
+
+            var responseText = $"You are searching for {education} course in {city}, right?";
+
+            var innerResponse = new PlainTextOutputSpeech
+            {
+                Text = responseText
+            };
+
+            return innerResponse;
+        }
+    }
+}
