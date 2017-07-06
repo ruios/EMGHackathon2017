5e0384112d8ba681d75a40f3037feb19055b0be9
Rui Deng <rui.deng@studentum.se>
2017-07-06 13:57:35 +0200

    New intenet for searching education only

---
+++ EmgAlexaHandler/EmgAlexaHandler/Intents/SearchWithOnlyEducationIntentHandler.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+using EmgAlexaHandler.Search;
+
+namespace EmgAlexaHandler.Intents
+{
+    public class SearchWithOnlyEducationIntentHandler : IIntentHandler
+    {
+        public bool CanHandle(string name)
+        {
+            return name == "SearchWithOnlyEducation";
+        }
+
+        public HandlerResult GetResponse(IntentRequest intentRequest, Session session)
+        {
+            var keyword = intentRequest.Intent.Slots["Education"].Value;
+
+            var client = new SearchClient();
+            var result = client.Search(keyword);
+
+            if (!result.Any())
+            {
+                return new HandlerResult()
+                {
+                    Response = new PlainTextOutputSpeech
+                    {
+                        Text = "Your search word sucked. Try again."
+                    }
+                };
+            }
+
+
+            var selectedResult = string.Join(", ", result.Select(i => $"{i.Name} from {i.Institutes.First().Name}"));
+            var responseText = $"We found {result.Count} results. Here are the top three: {selectedResult}. Are you happy with these results, or do you want to do a new search??";
+            
+            var innerResponse = new PlainTextOutputSpeech
+            {
+                Text = responseText
+            };
+
+            var attr = new Dictionary<string, object>()
+            {
+                { "EducationList", result.ToArray()}
+            };
+
+            return new HandlerResult()
+            {
+                Response = innerResponse, ResponseSessionAttributes = attr
+            };
+        }
+    }
+}
